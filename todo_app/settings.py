107a9d6f1cb60132abd8e44535df520d56905990
Emily McMullan <55964550+eemcmullan@users.noreply.github.com>
2020-10-18 12:19:25 -0400

    Update settings.py

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -23,18 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '79n=^$8$b9pybxu1p)!2!^bc#8&9^l@@(cne^sf%lu$-hebn@t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['emmctododj.herokuapp.com']
 
 
 # Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -45,6 +42,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
