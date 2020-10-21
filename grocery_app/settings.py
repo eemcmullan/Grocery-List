a8a91fc90deb04566f8f44966e4956e34f2e4837
Emily McMullan <55964550+eemcmullan@users.noreply.github.com>
2020-10-21 12:47:16 -0400

    Update settings.py

--- grocery_app/settings.py
+++ grocery_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '79n=^$8$b9pybxu1p)!2!^bc#8&9^l@@(cne^sf%lu$-hebn@t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
